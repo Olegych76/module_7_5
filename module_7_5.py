a5e14a8859c9a28b01f87fa3c41dfcb8c6e6027c
Olegych76 <f-o-r-j-o-b@yandex.ru>
2024-10-11 14:41:51 +0300

    Update module_7_5.py

--- module_7_5.py
+++ module_7_5.py
@@ -4,9 +4,9 @@ directory = '.'
 for root, dirs, files in os.walk(directory):
     for file in files:
         filepath = os.path.join(root, file)
-        filetime = os.path.getmtime(file)
+        parent_dir = os.path.dirname(filepath)
+        f_size = os.stat(filepath).st_size
+        filetime = os.path.getmtime(filepath)
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-        f_size = os.stat(file).st_size
-        parent_dir = os.path.abspath(os.path.join(root, os.pardir))
         print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {f_size} байт, '
               f'Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
